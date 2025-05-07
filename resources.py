0a512a5e94e8319c660911cd0e0c47d13c2959a6
Alyssa Landicho <alyssaxlandicho@gmail.com>
2025-05-07 17:50:26 +0800

    updated resources

--- resources.py
+++ resources.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # Constants
 SCREEN_W = 500
@@ -6,61 +7,85 @@ SCREEN_H = 720
 FPS = 60
 HEIGHT = 1000
 
+def load_image(path, alpha=True):
+    """Helper function to load images with optional alpha channel"""
+    img = pygame.image.load(path)
+    return img.convert_alpha() if alpha else img.convert()
+
+def load_sound(path, volume=1.0):
+    """Helper function to load and configure sounds"""
+    sound = pygame.mixer.Sound(path)
+    sound.set_volume(volume)
+    return sound
+
+def load_animation_frames(folder_path, frame_count, file_pattern="{}.png"):
+    """Load a sequence of animation frames from a folder"""
+    frames = []
+    for i in range(frame_count):
+        frame_path = os.path.join(folder_path, file_pattern.format(i))
+        frames.append(load_image(frame_path))
+    return frames
+
+def load_garbage_images(file_names, assets_folder="assets"):
+    """Load multiple garbage images from file names"""
+    return [load_image(os.path.join(assets_folder, fn)) for fn in file_names]
+
 def load_resources():
-    resources = {}
+    resources = {
+        'speed': 7,
+        'b_pos': 0,
+        'o_pos': SCREEN_H,
+        'height': HEIGHT
+    }
     
-    # Load images
-    resources['menu_logo_img'] = pygame.image.load('assets/logos_and_icons/menu_logo.png').convert_alpha()
-    resources['game_over_img'] = pygame.image.load('assets/logos_and_icons/game_over.png').convert_alpha()
-    resources['play_button_img'] = pygame.image.load('assets/logos_and_icons/play.png').convert_alpha()
-    resources['quit_button_img'] = pygame.image.load('assets/logos_and_icons/quit.png').convert_alpha()
-    resources['restart_button_img'] = pygame.image.load('assets/logos_and_icons/restart.png').convert_alpha()
-    resources['menu_button_img'] = pygame.image.load('assets/logos_and_icons/main_menu.png').convert_alpha()
+    # Load UI images
+    ui_images = {
+        'menu_logo_img': 'assets/logos_and_icons/menu_logo.png',
+        'game_over_img': 'assets/logos_and_icons/game_over.png',
+        'play_button_img': 'assets/logos_and_icons/play.png',
+        'quit_button_img': 'assets/logos_and_icons/quit.png',
+        'restart_button_img': 'assets/logos_and_icons/restart.png',
+        'menu_button_img': 'assets/logos_and_icons/main_menu.png'
+    }
+    for key, path in ui_images.items():
+        resources[key] = load_image(path)
     
-    # Background
-    resources['bg_image'] = pygame.image.load('assets/bg.png').convert()
-    resources['overlap_bg_image'] = pygame.image.load('assets/bg.png').convert()
-    resources['b_pos'] = 0
-    resources['o_pos'] = 720
-    resources['speed'] = 7
+    # Load backgrounds
+    resources['bg_image'] = load_image('assets/bg.png', alpha=False)
+    resources['overlap_bg_image'] = resources['bg_image'].copy()
     
-    # Lanes
-    resources['left_lane'] = 150
-    resources['center_lane'] = 166
-    resources['right_lane'] = 275
-    resources['lanes'] = [resources['left_lane'], resources['center_lane'], resources['right_lane']]
-    
-    resources['objectleft_lane'] = 130
-    resources['objectcenter_lane'] = 245
-    resources['objectright_lane'] = 360
+    # Configure lanes
+    resources.update({
+        'left_lane': 150,
+        'center_lane': 166,
+        'right_lane': 275,
+        'objectleft_lane': 130,
+        'objectcenter_lane': 245,
+        'objectright_lane': 360
+    })
+    resources['lanes'] = resources['left_lane'], resources['center_lane'], resources['right_lane']
     resources['objectlanes'] = [resources['objectleft_lane'], resources['objectcenter_lane'], resources['objectright_lane']]
     
-    # Height
-    resources['height'] = HEIGHT
+    # Load animations using helper function
+    resources['bio_animation'] = load_animation_frames('assets/wallyrunbio', 16)
+    resources['nonbio_animation'] = load_animation_frames('assets/wallyrunnonbio', 16)
     
-    # Load animations
-    resources['bio_animation'] = []
-    resources['nonbio_animation'] = []
-    for i in range(16):
-        resources['bio_animation'].append(pygame.image.load(f'assets/wallyrunbio/{i}.png'))
-        resources['nonbio_animation'].append(pygame.image.load(f'assets/wallyrunnonbio/{i}.png'))
-    
-    # Load garbage images
+    # Load garbage images using helper functions
     bio_filenames = ['banana peel.png', 'milk carton.png', 'box.png', 'Leaves.png', 'Poop.png', 
                     'Log.png', 'Book.png', 'Apple.png', 'Meat.png', 'Fishbone.png']
-    resources['biodegradable_images'] = [pygame.image.load('assets/' + fn) for fn in bio_filenames]
-    
     nonbio_filenames = ['plastic bag.png', 'soda bottle.png', 'water bottle.png', 'Battery.png', 
                        'Lightbulb.png', 'Phone.png', 'Laptop.png', 'Can.png', 'Soda.png', 'Glass.png']
-    resources['nonbiodegradable_images'] = [pygame.image.load('assets/' + fn) for fn in nonbio_filenames]
     
-    # Load sounds
+    resources['biodegradable_images'] = load_garbage_images(bio_filenames)
+    resources['nonbiodegradable_images'] = load_garbage_images(nonbio_filenames)
+    
+    # Initialize and load sounds
     pygame.mixer.init()
     resources['game_music'] = "assets/music/game_music.wav"
-    resources['get_item_sound'] = pygame.mixer.Sound("assets/music/get_item.mp3")
-    resources['game_over_sound'] = pygame.mixer.Sound("assets/music/game_over.mp3")
+    resources['get_item_sound'] = load_sound("assets/music/get_item.mp3", 0.5)
+    resources['game_over_sound'] = load_sound("assets/music/game_over.mp3", 0.5)
     
-    # Play music
+    # Configure and play music
     pygame.mixer.music.load(resources['game_music'])
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.2)
